2ed95667266d5ed2c1bb592c720c57f95308d099
Jendeuk <thekitty@live.no>
2021-12-29 20:26:18 +0100

    finalize design

--- calc.js
+++ calc.js
@@ -1,6 +1,26 @@
-window.onload = () => {
-  function
-  window.onload = () => {
-    function
-  };
-};
+(function () {
+  let screen = document.querySelector(".screen");
+  let buttons = document.querySelector(".btn");
+  let clear = document.querySelector(".btn-clear");
+  let equal = document.querySelector(".btn-equal");
+
+  buttons.forEach(function (button) {
+    button.addEvenListener("click", function (e) {
+      let value = e.target.dataset.num;
+      screen.value += value;
+    });
+  });
+
+  equal.addEventListener("click", function (e) {
+    if (screen.value === "") {
+      screen.value = "";
+    } else {
+      let answer = eval(screen.value);
+      screen.value = answer;
+    }
+  });
+
+  clear.addEventListener("click", function (e) {
+    screen.value = answer;
+  });
+})();
